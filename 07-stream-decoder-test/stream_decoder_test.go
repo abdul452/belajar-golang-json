4e80990b2c7fb9d2f156726a7932c69ef741c6a7
abdul452 <abdulsalimmkm@gmail.com>
2026-05-29 13:31:54 +0700

    stream-decoder

---
+++ 07-stream-decoder-test/stream_decoder_test.go
@@ -0,0 +1,41 @@
+package streamdecodertest
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"testing"
+)
+
+type Customer struct {
+	FirstName  string
+	MiddleName string
+	LastName   string
+}
+
+func TestStreamDecoder(t *testing.T) {
+	reader, err := os.Open("customer.json")
+	if err != nil {
+		panic(err)
+	}
+	decoder := json.NewDecoder(reader)
+
+	customer := &Customer{}
+	decoder.Decode(customer)
+	fmt.Println(customer)
+}
+
+func TestStreamDecoderMap(t *testing.T) {
+	reader, err := os.Open("customer.json")
+	if err != nil {
+		panic(err)
+	}
+	decoder := json.NewDecoder(reader)
+
+	var customer = map[string]any{}
+	err = decoder.Decode(&customer)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(customer)
+}
